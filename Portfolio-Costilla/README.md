80427f710a7ae938d4b90338f17156d903216bf9
Jorgelina79 <jorgelinacostilla@gmail.com>
2022-08-26 15:07:17 -0300

    Correccion titulo2

--- Portfolio-Costilla/README.md
+++ Portfolio-Costilla/README.md
@@ -1,4 +1,4 @@
-# PorfolioCostilla
+# PortfolioCostilla
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.0.
 
--- Portfolio-Costilla/src/app/app.component.spec.ts
+++ Portfolio-Costilla/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Porfolio-Costilla'`, () => {
+  it(`should have as title 'Portfolio-Costilla'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Porfolio-Costilla');
+    expect(app.title).toEqual('Portfolio-Costilla');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Porfolio-Costilla app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Portfolio-Costilla app is running!');
   });
 });
