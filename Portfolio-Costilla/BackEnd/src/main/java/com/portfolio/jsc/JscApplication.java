88b7bd168f19acffe6af20bab9e7d3bfad831a56
Jorgelina79 <jorgelinacostilla@gmail.com>
2022-10-26 22:33:25 -0300

    CarpetaBackEnd

---
+++ Portfolio-Costilla/BackEnd/src/main/java/com/portfolio/jsc/JscApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JscApplication.class, args);
+	}
+
+}
---
+++ Portfolio-Costilla/BackEnd/src/test/java/com/portfolio/jsc/JscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.jsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
