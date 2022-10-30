1cc518c769fa648f9632c6f5a0598a9986b2f279
Jorgelina79 <jorgelinacostilla@gmail.com>
2022-10-30 19:40:42 -0300

    Routing

--- Portfolio-Costilla/FrontEnd/src/app/app.module.ts
+++ Portfolio-Costilla/FrontEnd/src/app/app.module.ts
@@ -14,6 +14,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { HttpClient, HttpClientModule} from '@angular/common/http';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
@@ -27,6 +30,9 @@ import { HttpClient, HttpClientModule} from '@angular/common/http';
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
     ],
   imports: [
     BrowserModule,
